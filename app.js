fbc7d1d2c4d74451e5364a4564f18b654f80de25
WebsiteServer1 <155986498+WebsiteServer1@users.noreply.github.com>
2024-01-08 21:31:31 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'username',
+    password: 'password',
+    database: 'dbname'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id ' + connection.threadId);
+});
